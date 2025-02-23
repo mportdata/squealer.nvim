68f10ef2bb57e7f33efec0ca120f9f74abfd3d34
mikexporter <mikexporter@gmail.com>
2025-02-23 21:49:37 +0000

    now have to select both source and target dialects

--- lua/squealer.lua
+++ lua/squealer.lua
@@ -7,59 +7,50 @@ local action_state = require("telescope.actions.state")
 
 local dialects = { "snowflake", "hive", "trino", "spark", "bigquery", "duckdb" }
 
-function M.pick_source_dialect()
+function M.pick_source_dialect(callback)
 	pickers
 		.new({}, {
 			prompt_title = "Select Source SQL Dialect",
 			finder = finders.new_table(dialects),
 			sorter = require("telescope.config").values.generic_sorter({}),
 			attach_mappings = function(_, map)
-				local source_selection = map("i", "<CR>", function(bufnr)
-					local source_selection = action_state.get_selected_entry()
+				map("i", "<CR>", function(bufnr)
+					local source_selection = action_state.get_selected_entry()[1]
 					actions.close(bufnr)
-					return source_selection
+					callback(source_selection)
 				end)
-				return source_selection
+				return true
 			end,
 		})
 		:find()
 end
 
-function M.pick_target_dialect()
+function M.pick_target_dialect(source_dialect)
 	pickers
 		.new({}, {
 			prompt_title = "Select Target SQL Dialect",
 			finder = finders.new_table(dialects),
 			sorter = require("telescope.config").values.generic_sorter({}),
 			attach_mappings = function(_, map)
-				local target_selection = map("i", "<CR>", function(bufnr)
-					local target_selection = action_state.get_selected_entry()
+				map("i", "<CR>", function(bufnr)
+					local target_selection = action_state.get_selected_entry()[1]
 					actions.close(bufnr)
-					return target_selection
+					vim.api.nvim_command("TranspileSQL " .. source_dialect .. " " .. target_selection)
 				end)
-				return target_selection
+				return true
 			end,
 		})
 		:find()
 end
 
 function M.transpile_sql()
-	local source_selection = M.pick_source_dialect()[1]
-	local target_selection = M.pick_target_dialect()[1]
-	if not source_selection then
-		print("Source selection missing!")
-	else
-		if not target_selection then
-			print("Target selection missing!")
-		else
-			vim.api.nvim_command("TranspileSQL " .. source_selection .. target_selection)
-		end
-	end
-	return true
+	M.pick_source_dialect(function(source_dialect)
+		M.pick_target_dialect(source_dialect)
+	end)
 end
 
 function M.setup()
-	vim.api.nvim_create_user_command("SquealerPickDialect", M.transpile_sql(), {})
+	vim.api.nvim_create_user_command("SquealerPickDialect", M.transpile_sql, {}) -- Pass function reference, not call
 	print("Squealer plugin loaded successfully!")
 end
 
