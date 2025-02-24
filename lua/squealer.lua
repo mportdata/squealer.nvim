460f746a5301dc929bb3be4814841e112628c620
mikexporter <mikexporter@gmail.com>
2025-02-24 17:00:50 +0000

    added correct quotes on lua variables

--- lua/squealer.lua
+++ lua/squealer.lua
@@ -36,10 +36,11 @@ function M.pick_target_dialect(source_dialect)
 					local target_selection = action_state.get_selected_entry()[1]
 					actions.close(bufnr)
 					vim.api.nvim_command(
-						"TranspileSQL "
+						"TranspileSQL '"
 							.. vim.fn.shellescape(source_dialect)
-							.. " "
+							.. "' '"
 							.. vim.fn.shellescape(target_selection)
+							.. "'"
 					)
 				end)
 				return true
