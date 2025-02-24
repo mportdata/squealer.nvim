58955c549f15784d57f261363fde24c139387562
mikexporter <mikexporter@gmail.com>
2025-02-24 17:11:48 +0000

    added way to deal with multiple arguements in python

--- rplugin/python3/squealer.py
+++ rplugin/python3/squealer.py
@@ -13,30 +13,36 @@ class SqlTranspiler:
         if not args or len(args) < 2:    
             self.nvim.command("SquealerPickDialect")
         else:
-            self._run_transpiler(args[0], args[1])
+            self._run_transpiler(args[0]) 
 
-    def _run_transpiler(self, source_dialect, target_dialect):
-        """Handles SQL transpilation"""
-        current_file = self.nvim.current.buffer.name
-        if not current_file:
-            self.nvim.err_write("No file open.\n")
-            return
+    def _run_transpiler(self, dialects):
+        dialects_array = dialects.split(' ')
+        if len(dialects_array) < 2:
+            self.nvim.err_write("Number of dialects detected less than 2")
+        else:
+            source_dialect = dialects_array[0]
+            target_dialect = dialects_array[1]
+            """Handles SQL transpilation"""
+            current_file = self.nvim.current.buffer.name
+            if not current_file:
+                self.nvim.err_write("No file open.\n")
+                return
 
-        try:
-            # Read from buffer instead of file for latest changes
-            sql = "\n".join(self.nvim.current.buffer[:])
-            transpiled_sql = sqlglot.transpile(
-                sql, 
-                read=source_dialect, 
-                write=target_dialect,
-                pretty=True
-            )[0]
+            try:
+                # Read from buffer instead of file for latest changes
+                sql = "\n".join(self.nvim.current.buffer[:])
+                transpiled_sql = sqlglot.transpile(
+                    sql, 
+                    read=source_dialect, 
+                    write=target_dialect,
+                    pretty=True
+                )[0]
 
-            output_file = f"{os.path.splitext(current_file)[0]}.sql"
-            with open(output_file, "w") as f:
-                f.write(transpiled_sql)
-            self.nvim.command("redraw!")
-            self.nvim.out_write(f"SQL transpiled from {source_dialect} to {target_dialect}.\n")
-        except Exception as e:
-            self.nvim.err_write(f"Unexpected error: {e}\n")
+                output_file = f"{os.path.splitext(current_file)[0]}.sql"
+                with open(output_file, "w") as f:
+                    f.write(transpiled_sql)
+                self.nvim.command("redraw!")
+                self.nvim.out_write(f"SQL transpiled from {source_dialect} to {target_dialect}.\n")
+            except Exception as e:
+                self.nvim.err_write(f"Unexpected error: {e}\n")
 
